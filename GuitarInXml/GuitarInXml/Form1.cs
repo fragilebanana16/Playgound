6478e45eb8ecbc3f02fcd02054c8932d38a4b14c
fragilebanana16 <850604714@qq.com>
2022-04-03 20:30:07 +0800

    浅浅的看下如何把谱子放到XML然后转换成对象然后用LINQ查询这档子事

---
+++ GuitarInXml/GuitarInXml/Form1.cs
@@ -0,0 +1,147 @@
+﻿// [Title]
+// 《XML to Obejct/ Object to XML》
+// [Blog]
+// https://www.codeproject.com/Articles/1163664/Convert-XML-to-Csharp-Object
+// [Brief]
+//                                ***XML与Object之间的转换***
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace GuitarInXml
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Test();
+        }
+
+        public void Test()
+        {
+            Serializer ser = new Serializer();
+            string path = string.Empty;
+            string xmlInputData = string.Empty;
+            string xmlOutputData = string.Empty;
+
+            path = Directory.GetCurrentDirectory() + @"\TEST.xml"; // Put xml with exe file same level
+            xmlInputData = File.ReadAllText(path, Encoding.UTF8);
+
+            // TODO:check if empty
+
+            Guitar customer = ser.Deserialize<Guitar>(xmlInputData);
+            xmlOutputData = ser.Serialize<Guitar>(customer);
+            File.WriteAllText(Directory.GetCurrentDirectory() + @"\Output.xml", xmlOutputData, Encoding.UTF8);
+        }
+    }
+
+    /// <summary>
+    /// Serialize
+    /// </summary>
+    public class Serializer
+    {
+        /// <summary>
+        /// Deserialize
+        /// </summary>
+        /// <typeparam name="T">T</typeparam>
+        /// <param name="input">content</param>
+        /// <returns>T</returns>
+        public T Deserialize<T>(string input) where T : class
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(T));
+
+            using (StringReader sr = new StringReader(input))
+            {
+                return (T)ser.Deserialize(sr);
+            }
+        }
+
+        /// <summary>
+        /// Serialize
+        /// </summary>
+        /// <typeparam name="T">T</typeparam>
+        /// <param name="ObjectToSerialize">object</param>
+        /// <returns></returns>
+        public string Serialize<T>(T ObjectToSerialize)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(ObjectToSerialize.GetType());
+
+            using (StringWriter textWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(textWriter, ObjectToSerialize);
+                return textWriter.ToString();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Type
+    /// </summary>
+    public class Guitar
+    {
+        public List<Song> Songs { get; set; }
+    }
+
+    /// <summary>
+    /// Single song
+    /// </summary>
+    public class Song
+    {
+        /// <summary>
+        /// Author
+        /// </summary>
+        public string Author { get; set; }
+
+        /// <summary>
+        /// Song`s Name
+        /// </summary>
+        public string SongName { get; set; }
+
+        /// <summary>
+        /// Capo on fret
+        /// </summary>
+        public string Capo { get; set; }
+
+        /// <summary>
+        /// Progression used
+        /// </summary>
+        public string Chords { get; set; }
+
+        /// <summary>
+        /// Six strings
+        /// </summary>
+        public List<TabString> TabStrings { get; set; }
+
+        /// <summary>
+        /// Addition Description
+        /// </summary>
+        public string Tips { get; set; }
+
+        /// <summary>
+        /// RecordDate
+        /// </summary>
+        public DateTime RecordDate { get; set; }
+    }
+
+    /// <summary>
+    /// Single string
+    /// </summary>
+    public class TabString
+    {
+        /// <summary>
+        /// 1-6 string
+        /// </summary>
+        public string StringLine { get; set; }
+    }
+}
