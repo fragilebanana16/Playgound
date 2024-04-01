31f05413c3744dbd66ee3f913310ca439831e21c
fragilebanana16 <850604714@qq.com>
2024-04-01 21:05:55 +0800

    [MVVM]资产概要控件，ControlTemplate 典型写法为设置Template属性，再用ControlTemplate给到Value
                                    <Setter Property="Template">
                                        <Setter.Value>
                                            <ControlTemplate TargetType="GridViewColumnHeader">
                                                <Border BorderBrush="Gray" BorderThickness="0 0 0 1" Padding="0 5 50 5">
                                                    <TextBlock Text="{TemplateBinding Content}"/>
                                                </Border>
                                            </ControlTemplate>
                                        </Setter.Value>
                                    </Setter>

--- MVVM/SimpleTrader.Domain/SimpleTrader.WPF/ViewModels/AssetSummaryViewModel.cs
+++ MVVM/SimpleTrader.Domain/SimpleTrader.WPF/ViewModels/AssetSummaryViewModel.cs
@@ -11,18 +11,17 @@ namespace SimpleTrader.WPF.ViewModels
     public class AssetSummaryViewModel : ViewModelBase
     {
         private readonly AssetStore _assetStore;
-        public readonly ObservableCollection<AssetViewModel> _assets;
+        public readonly ObservableCollection<AssetViewModel> _topAssets;
 
         public double AccountBalance => _assetStore.AccountBalance;
-        public IEnumerable<AssetViewModel> Assets => _assets;
+        public IEnumerable<AssetViewModel> TopAssets => _topAssets; // readonly, UEnumerable has no add, remove etc. methods
 
         public AssetSummaryViewModel(AssetStore assetStore)
         {
             _assetStore = assetStore;
-            _assets = new ObservableCollection<AssetViewModel>();
+            _topAssets = new ObservableCollection<AssetViewModel>();
             _assetStore.StateChanged += AssetStore_StateChanged;
             ResetAssets();
-
         }
 
         private void AssetStore_StateChanged()
@@ -36,11 +35,13 @@ namespace SimpleTrader.WPF.ViewModels
             IEnumerable<AssetViewModel> assetViewModels = _assetStore.AssetTransactions
                 .GroupBy(t => t.Asset.Symbol)
                 .Select(g => new AssetViewModel(g.Key, g.Sum(a => a.IsPurchase ? a.Shares : -a.Shares)))
-                .Where(a => a.Shares > 0);
-            _assets.Clear();
+                .Where(a => a.Shares > 0)
+                .OrderByDescending(a => a.Shares)
+                .Take(3);
+            _topAssets.Clear();
             foreach (var assetViewModel in assetViewModels)
             {
-                _assets.Add(assetViewModel);
+                _topAssets.Add(assetViewModel);
             }
         }
     }
--- MVVM/SimpleTrader.Domain/SimpleTrader.WPF/ViewModels/AssetViewModel.cs
+++ MVVM/SimpleTrader.Domain/SimpleTrader.WPF/ViewModels/AssetViewModel.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace SimpleTrader.WPF.ViewModels
 {
-    public class AssetViewModel : ViewModelBase
+    public class AssetViewModel : ViewModelBase // inherient viewModelBase to avoid memory leaks? Binding ui to non-property change class may leads to leak
     {
         public string Symbol { get; }
         public int Shares { get; }
