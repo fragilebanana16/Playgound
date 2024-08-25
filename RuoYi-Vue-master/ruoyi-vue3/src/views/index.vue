b4603204c39c93c611813f4d057024736530583a
fragilebanana16 <850604714@qq.com>
2024-08-25 17:58:31 +0800

    [ruoyi-vue]=========================== 及时止损，切换为Vue3 ===============================

---
+++ RuoYi-Vue-master/ruoyi-vue3/src/views/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app-container home">
+    None
+  </div>
+</template>
+
+<script setup name="Index">
+const version = ref('3.8.8')
+
+function goTarget(url) {
+  window.open(url, '__blank')
+}
+</script>
+
+<style scoped lang="scss">
+.home {
+
+}
+</style>
+
