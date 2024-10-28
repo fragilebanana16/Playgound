e2dafb0ea406bf91570d2573e2f634e6ee07e0f7
fragilebanana16 <850604714@qq.com>
2024-10-28 22:25:58 +0800

    [ThreeJS]真实的WebGLCubeRenderTarget、全景

--- ThreeJS/OrbitControlGlbModel/js/main.js
+++ ThreeJS/OrbitControlGlbModel/js/main.js
@@ -13,10 +13,18 @@ import * as THREE from 'three';
 import { GLTFLoader } from "../utils/loaders/GLTFLoader.js";
 import { DRACOLoader } from "../utils/loaders/DRACOLoader.js";
 import { OrbitControls } from "../utils/controls/OrbitControls.js";
+import { Depth, LayerMaterial } from "../utils/vanilla/vanilla.js";
 
 //Create a Three.JS Scene
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xbbbbbb)
+
+let backLightColor = "#2de8cd";
+// 虚拟场景对象
+let virtualScene = new THREE.Scene();
+// 虚拟背景
+let virtualBackgroundMesh;
+
 //create a new camera with positions and angles
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
@@ -59,12 +67,14 @@ const getMaterials = (scene) => {
   return data;
 };
 
+
+
 /**
  * Format1: Load glb with gltfLoader
  */
 const dracoLoader = new DRACOLoader().setDecoderPath("../utils/draco/");
 const gltfLoader = new GLTFLoader().setDRACOLoader(dracoLoader);
-const model = await gltfLoader.loadAsync("models/lamborghini_centenario_roadster_sdc.glb");
+const model = await gltfLoader.loadAsync("models/911.glb");
 if (model.scene) {
   const { materials, nodes } = getMaterials(model.scene);
 
@@ -125,7 +135,7 @@ document.getElementById("container3D").appendChild(renderer.domElement);
 camera.position.z = 5;
 camera.position.y = 20;
 
-const topLight = new THREE.DirectionalLight(0xffffff, 10); // (color, intensity)
+const topLight = new THREE.DirectionalLight(0xffffff, 9); // (color, intensity)
 topLight.position.set(10, 10, 10);
 topLight.castShadow = true;
 scene.add(topLight);
@@ -136,8 +146,12 @@ const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 }); // 使用 
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-const ambientLight = new THREE.AmbientLight(0x404040, 40);
+const ambientLight = new THREE.AmbientLight(0x404040, 1);
 scene.add(ambientLight);
+
+const hemiLight = new THREE.HemisphereLight(0xffeeb1, 0x080820, 4);
+scene.add(hemiLight);
+
 
 controls = new OrbitControls(camera, renderer.domElement);
 const clock = new THREE.Clock();
@@ -173,6 +187,188 @@ document.onmousemove = (e) => {
   mouseX = e.clientX;
   mouseY = e.clientY;
 }
+const DPR = window.devicePixelRatio;
 
+const setRender = () => {
+  renderer.shadowMap.enabled = true;
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(DPR);
+
+  // GLTFLoader 将自动配置从 .gltf 或 .glb 文件引用的纹理。
+  renderer.outputEncoding = THREE.sRGBEncoding;
+
+  //是否使用物理上正确的光照模式。默认为false。
+  // renderer.physicallyCorrectLights = true;
+
+  renderer.toneMapping = THREE.ACESFilmicToneMapping;
+  renderer.toneMappingExposure = 1;
+
+  // BakeShadows
+  renderer.shadowMap.autoUpdate = false;
+  renderer.shadowMap.needsUpdate = true;
+};
+export const generateVirtualLight = ({
+  form = "rect",
+  intensity = 1,
+  color = "white",
+  scale = [1, 1, 1],
+  toneMapped = false,
+  position = [0, 0, 0],
+  rotation = [0, 0, 0],
+  target,
+}) => {
+  scale =
+    Array.isArray(scale) && scale.length === 2
+      ? [scale[0], scale[1], 1]
+      : scale;
+
+  let geometry;
+  if (form === "circle") {
+    geometry = new THREE.RingGeometry(0, 1, 64);
+  }
+  if (form === "ring") {
+    geometry = new THREE.RingGeometry(0.5, 1, 64);
+  }
+  if (form === "rect") {
+    geometry = new THREE.PlaneGeometry();
+  }
+
+  const material = new THREE.MeshBasicMaterial({
+    toneMapped: toneMapped,
+    side: THREE.DoubleSide,
+    color: color,
+  });
+
+  material.color.multiplyScalar(intensity);
+
+  const mesh = new THREE.Mesh(geometry, material);
+  Array.isArray(scale)
+    ? mesh.scale.set(...scale)
+    : mesh.scale.set(scale, scale, scale);
+
+  mesh.position.set(...position);
+  mesh.rotation.set(...rotation);
+  if (target) {
+    mesh.lookAt(new THREE.Vector3(...target));
+  }
+  return mesh;
+};
+
+export const createCustomMaterial = (colorA = "white", colorB = "black") => {
+  const material = new LayerMaterial({
+    color: "#444",
+    layers: [
+      new Depth({
+        colorA: colorA,
+        colorB: colorB,
+        alpha: 0.5,
+        mode: "normal",
+        near: 0,
+        far: 300,
+        origin: new THREE.Vector3(100, 100, 100),
+      }),
+    ],
+  });
+  material.side = THREE.BackSide;
+  return material;
+};
+
+const setEnvironment = (
+  scene,
+  resolution = 256,
+  frames = 1,
+  near = 1,
+  far = 1000,
+  background = false
+) => {
+  const fbo = new THREE.WebGLCubeRenderTarget(resolution);
+  fbo.texture.type = THREE.HalfFloatType;
+  const cubeCamera = new THREE.CubeCamera(near, far, fbo);
+
+  virtualScene.add(cubeCamera);
+
+  // 天花板灯
+  const topLight = generateVirtualLight({
+    intensity: 0.75,
+    scale: [10, 10, 1],
+    position: [0, 5, -9],
+    rotation: [Math.PI / 2, 0, 0],
+  });
+
+  // 四周灯光
+  const leftTopLight = generateVirtualLight({
+    intensity: 4,
+    scale: [20, 0.1, 1],
+    position: [-5, 1, -1],
+    rotation: [0, Math.PI / 2, 0],
+  });
+  const leftBottomLight = generateVirtualLight({
+    intensity: 1,
+    scale: [20, 0.5, 1],
+    position: [-5, -1, -1],
+    rotation: [0, Math.PI / 2, 0],
+  });
+  const rightTopLight = generateVirtualLight({
+    intensity: 1,
+    scale: [20, 1, 1],
+    position: [10, 1, 0],
+    rotation: [0, -Math.PI / 2, 0],
+  });
+
+  const floatLight = generateVirtualLight({
+    form: "ring",
+    color: "red",
+    intensity: "1",
+    scale: 10,
+    position: [-15, 4, -18],
+    target: [0, 0, 0],
+  });
+
+  virtualScene.add(topLight);
+  virtualScene.add(leftTopLight);
+  virtualScene.add(leftBottomLight);
+  virtualScene.add(rightTopLight);
+  virtualScene.add(floatLight);
+
+  if (background !== "only") {
+    scene.environment = fbo.texture;
+  }
+  if (background) {
+    scene.background = fbo.texture;
+  }
+
+  // 模拟 MeshDepthMaterial 设置背景颜色，颜色不受光照影响
+
+  const geometry = new THREE.SphereGeometry(1, 64, 64);
+  const material = createCustomMaterial(backLightColor);
+
+  virtualBackgroundMesh = new THREE.Mesh(geometry, material);
+  virtualBackgroundMesh.scale.set(100, 100, 100);
+  virtualScene.add(virtualBackgroundMesh);
+
+  // 让环形网格运动起来
+  // floatMesh({
+  //   group: floatLight,
+  //   speed: 5,
+  //   rotationIntensity: 2,
+  //   floatIntensity: 2,
+  // });
+
+  // 更新相机内容
+  let count = 1;
+  const virtualRender = () => {
+    if (frames === Infinity || count < frames) {
+      cubeCamera.update(renderer, virtualScene);
+      count++;
+    }
+    requestAnimationFrame(virtualRender);
+  };
+  virtualRender();
+};
+
+
+setRender();
+setEnvironment(scene, 256, Infinity);
 //Start the 3D rendering
 animate();
+
