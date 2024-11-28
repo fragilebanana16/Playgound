3bdcb43234986881827b2e7d4516d579063c216a
fragilebanana16 <850604714@qq.com>
2024-11-28 23:11:01 +0800

    [ruoyi-vue] 相册v-viewer预览自定义图标：下载、删除

--- RuoYi-Vue-master/ruoyi-vue3/src/views/media/album/components/Photo.vue
+++ RuoYi-Vue-master/ruoyi-vue3/src/views/media/album/components/Photo.vue
@@ -1,10 +1,11 @@
 <template>
     <div>
         <Icon v-if="data.is_video" icon='iconamoon:folder-video-fill' class="text-xl text-white icon-video-white"></Icon>
-        <img @click="show(collection, data.url)" :src="data.ph ? undefined : data.url" :key="data.fileid" @error="handleImageError" alt="mountains" v-bind:style="{
-            width: rowHeight + 'px',
-            height: rowHeight + 'px',
-        }" />
+        <img @click="show(collection, data.url)" :src="data.ph ? undefined : data.url" :key="data.fileid"
+            @error="handleImageError" alt="mountains" v-bind:style="{
+                width: rowHeight + 'px',
+                height: rowHeight + 'px',
+            }" />
     </div>
 </template>
 <script>
@@ -29,6 +30,10 @@ export default {
         }
     },
     methods: {
+        getCurrentImage() {
+            const currentImage = this.collection[0]; // todo get index?
+            return currentImage;
+        },
         /**
          * show v-viewer
          * @param photos one row photos, todo: same day photos
@@ -44,7 +49,36 @@ export default {
             this.$viewerApi({
                 images: urls,
                 options: {
-                    initialViewIndex: curIndex
+                    initialViewIndex: curIndex,
+                    toolbar: {
+                        zoomIn: 4,
+                        zoomOut: 4,
+                        oneToOne: 4,
+                        reset: 4,
+                        prev: 4,
+                        play: {
+                            show: 4,
+                            size: 'large',
+                        },
+                        next: 4,
+                        rotateLeft: 4,
+                        rotateRight: 4,
+                        flipHorizontal: 4,
+                        flipVertical: 4,
+                        download: () => {
+                            const curImg = this.getCurrentImage()
+                            const a = document.createElement('a');
+                            a.href = curImg;
+                            const filename = curImg.url.split('/').pop();
+                            a.download = filename;
+                            document.body.appendChild(a);
+                            a.click();
+                            document.body.removeChild(a);
+                        },
+                        delete: () => {
+                            console.log(`output->delete`)
+                        },
+                    },
                 },
             })
         },
@@ -69,4 +103,60 @@ img {
     border-radius: 3%;
     cursor: pointer;
 }
-</style>
+
+.viewer-download,
+.viewer-delete {
+    color: #fff;
+    font-family: FontAwesome, serif;
+    font-size: 0.75rem;
+    line-height: 1.2rem;
+    text-align: center;
+}
+
+.viewer-download::before {
+    content: "\2b07";
+}
+
+.viewer-delete::before {
+    content: "\1F5D1";
+}
+
+/* 悬浮提示样式 */
+.viewer-download::after,
+.viewer-delete::after {
+    position: absolute;
+    /* todo点击后位置变化? */
+    left: 60%;
+    /* 水平居中 */
+    background-color: rgba(0, 0, 0, 0.7);
+    /* 半透明黑色背景 */
+    color: #fff;
+    /* 白色文字 */
+    padding: 5px;
+    border-radius: 4px;
+    visibility: hidden;
+    /* 默认隐藏 */
+    opacity: 0;
+    /* 隐藏状态 */
+    transition: opacity 0.2s, visibility 0.2s;
+    /* 动画效果 */
+    white-space: nowrap;
+    /* 防止换行 */
+}
+
+/* 特定内容 */
+.viewer-download::after {
+    content: "下载";
+}
+
+.viewer-delete::after {
+    content: "删除";
+}
+
+.viewer-download:hover::after,
+.viewer-delete:hover::after {
+    visibility: visible;
+    /* 悬停时可见 */
+    opacity: 1;
+    /* 悬停时显示 */
+}</style>
