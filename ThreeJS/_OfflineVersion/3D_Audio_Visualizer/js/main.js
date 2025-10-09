8c2b402cfe38a2a54af7ff6ba9115dd58cf4384a
fragilebanana16 <850604714@qq.com>
2025-10-09 23:03:44 +0800

    [ThreeJS]离线版音频可视化-时间变量 https://waelyasmina.net/articles/how-to-create-a-3d-audio-visualizer-using-three-js/#preparations

---
+++ ThreeJS/_OfflineVersion/3D_Audio_Visualizer/js/main.js
@@ -0,0 +1,59 @@
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Sets the color of the background.
+// renderer.setClearColor(0xfefefe);
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+// Sets orbit control to move the camera around.
+const orbit = new THREE.OrbitControls(camera, renderer.domElement);
+
+// Camera positioning.
+camera.position.set(6, 8, 14);
+// Has to be done everytime we update the camera position.
+orbit.update();
+
+const uniforms = {
+  u_time: { value: 0.0 },
+};
+
+const mat = new THREE.ShaderMaterial({
+  wireframe: true,
+  uniforms,
+  vertexShader: document.getElementById('vertexshader').textContent,
+  fragmentShader: document.getElementById('fragmentshader').textContent,
+});
+
+const geo = new THREE.IcosahedronGeometry(4, 10);
+const mesh = new THREE.Mesh(geo, mat);
+scene.add(mesh);
+
+// Creates a 12 by 12 grid helper.
+const gridHelper = new THREE.GridHelper(12, 12);
+scene.add(gridHelper);
+
+// Creates an axes helper with an axis length of 4.
+const axesHelper = new THREE.AxesHelper(4);
+scene.add(axesHelper);
+
+const clock = new THREE.Clock();
+function animate() {
+  uniforms.u_time.value = clock.getElapsedTime();
+  renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+
+window.addEventListener('resize', function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
