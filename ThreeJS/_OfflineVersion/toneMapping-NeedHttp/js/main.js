c4abea75e0f5d1037f31ea346e7b556be6aeefbc
fragilebanana16 <850604714@qq.com>
2025-10-13 23:04:53 +0800

    [ThreeJS]toneMapping 增加python服务器避免跨域资源获取

---
+++ ThreeJS/_OfflineVersion/toneMapping-NeedHttp/js/main.js
@@ -0,0 +1,44 @@
+
+// 创建场景
+const scene = new THREE.Scene();
+
+// 创建相机
+const camera = new THREE.PerspectiveCamera(
+  75, window.innerWidth / window.innerHeight, 0.1, 1000
+);
+camera.position.z = 3;
+
+// 创建渲染器
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// 创建立方体
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshNormalMaterial(); // 不依赖光照
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+const rgbeLoader = new THREE.RGBELoader();
+
+rgbeLoader.load('/toneMapping-NeedHttp/sunny_prairie_expanse_2k.hdr', function (texture) {
+  scene.background = texture;
+});
+
+
+const orbit = new THREE.OrbitControls(camera, renderer.domElement);
+
+orbit.update();
+
+// 响应窗口大小变化
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+// 动画循环
+function animate() {
+  renderer.render(scene, camera);
+}
+renderer.setAnimationLoop(animate);
