8d56db7852023fef31285e348b44532a412382a3
fragilebanana16 <850604714@qq.com>
2025-11-02 22:30:55 +0800

    [WebGL]离线版shader球体表面动态扭曲 shader库https://github.com/patriciogonzalezvivo/lygia/tree/main

---
+++ ThreeJS/_OfflineVersion/shaderCnoise/js/main.js
@@ -0,0 +1,67 @@
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Sets the color of the background.
+// renderer.setClearColor(0xfefefe);
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+// Sets orbit control to move the camera around.
+const orbit = new THREE.OrbitControls(camera, renderer.domElement);
+
+// Camera positioning.
+camera.position.set(6, 8, 14);
+// Has to be done everytime we update the camera position.
+orbit.update();
+
+const uniforms = {
+	iTime: { value: 0 },
+	iResolution: { value: new THREE.Vector3(window.innerWidth, window.innerHeight, 1) },
+	iMouse: { value: new THREE.Vector4() },
+	uDistort: { value: 1.0 },
+}
+
+const vertexShader = document.getElementById("vertexShader").textContent;
+const fragmentShader = document.getElementById("fragmentShader").textContent;
+const mat = new THREE.ShaderMaterial({
+  wireframe: true,
+  uniforms,
+  vertexShader: vertexShader ,
+  fragmentShader: fragmentShader,
+});
+
+const geometry = new THREE.SphereGeometry(2, 64, 64);
+const meshShader = new THREE.Mesh(geometry, mat);
+scene.add(meshShader);
+
+
+// Creates a 12 by 12 grid helper.
+const gridHelper = new THREE.GridHelper(12, 12);
+scene.add(gridHelper);
+
+// Creates an axes helper with an axis length of 4.
+const axesHelper = new THREE.AxesHelper(4);
+scene.add(axesHelper);
+
+const clock = new THREE.Clock();
+function animate() {
+  uniforms.iTime.value = clock.getElapsedTime();
+  uniforms.iResolution.value.set(window.innerWidth, window.innerHeight, 1);
+  // material.uniforms.iMouse.value.set(x, y, 0, 0);
+  renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+
+window.addEventListener('resize', function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
