68e33f04d22815e47fd7bff1dca918d1b0875b52
fragilebanana16 <850604714@qq.com>
2025-12-25 23:54:41 +0800

    [OpenGL]抗锯齿-开启办法 取决于Nvidia控制面板设置【性能】或是【质量】

--- OpenGL/vs/Demo/EmptyWindow/main.cpp
+++ OpenGL/vs/Demo/EmptyWindow/main.cpp
@@ -31,7 +31,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
+    glfwWindowHint(GLFW_SAMPLES, 4);
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
@@ -45,6 +45,7 @@ int main()
         glfwTerminate();
         return -1;
     }
+
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     glfwSetCursorPosCallback(window, mouse_callback);
@@ -56,8 +57,9 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-
+    glEnable(GL_MULTISAMPLE);
     glEnable(GL_DEPTH_TEST);
+
     //glEnable(GL_CULL_FACE);
     //       2________3
     //       /|      /|
